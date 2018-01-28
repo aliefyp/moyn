3ced18d2b683ed5ef718a19b6f33634a06b81112
Nakama <aliefyp@gmail.com>
2018-01-28 10:30:59 +0700

    responsive & slider

--- application/views/home.php
+++ application/views/home.php
@@ -13,11 +13,22 @@
 	</head>
 
 	<body>
-		<div class="container moyn__container">
-			<div class="moyn__header"><img src="http://i65.tinypic.com/2rny2pu.png" alt="moyn-logo"></div>
-			<ul class="moyn__menu">
-				<li class="moyn__menu-item" id="profile">PROFILE
-					<div class="moyn__menu-content">
+		<div class="container relative">
+			<div class="moyn__header">
+				<img src="http://i65.tinypic.com/2rny2pu.png" alt="moyn-logo">
+				<!-- hamburger (mobile) -->
+				<nav class="navbar navbar-dark moyn__navbar--mobile ">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+				</nav>
+				<!-- end of hamburger -->
+			</div>
+
+			<!-- menu desktop -->
+			<div class="moyn__menu">
+				<div class="moyn__menu-item" id="profile">PROFILE
+					<div class="moyn__menu-content" id="content-profile">
 						<p>Established in 2018, moyn is an architectural design studio based on the countryside of central Java.<br />
 						the balance between spaces inside and outside, environment and architecture, human and architecture.<br />
 						moyn architecture is seeking for equilibrium, a pursuit of idea to stand between them</p>
@@ -44,12 +55,12 @@
 							</div>
 						</div>
 					</div>
-				</li>
-				<li class="moyn__menu-item" id="projects">PROJECTS
-					<div class="moyn__content-nav justify-content-end">
+				</div>
+				<div class="moyn__menu-item" id="projects">PROJECTS
+					<div class="moyn__content-nav justify-content-end" id="content-project">
 						<div class="proj-nav">
 							<div class="proj-nav__item" id="nav-unbuilt" >&ndash;</div>
-							<div class="proj-nav__item" id="nav-studio" >0</div>
+							<div class="proj-nav__item active" id="nav-studio" >0</div>
 							<div class="proj-nav__item" id="nav-realized" >+</div>
 						</div>
 					</div>
@@ -59,9 +70,9 @@
 						<div class="proj-next" id="gallery-next">&gt;</div>
 						<div class="proj-prev" id="gallery-prev">&lt;</div>
 					</div>
-				</li>
-				<li class="moyn__menu-item" id="contact">CONTACT
-					<div class="moyn__menu-content ta-right">
+				</div>
+				<div class="moyn__menu-item" id="contact">CONTACT
+					<div class="moyn__menu-content ta-right" id="content-contact">
 						<div class="mb-16">
 							<div>Jl.Carikan Gondosuli</div>
 							<div>Muntailan Magelang, 56415</div>
@@ -73,9 +84,10 @@
 							<div>studio.moyn@gmail.com</div>
 						</div>
 					</div>
-				</li>
-				<li class="moyn__menu-item" id="news">NEWS</li>
-				<li class="moyn__menu-item" id="shop">SHOP
+				</div>
+				<div class="moyn__menu-item" id="news">NEWS
+				</div>
+				<div class="moyn__menu-item" id="shop">SHOP
 					<div class="moyn__menu-content">
 						<div class="moyn__gallery" id="gallery-unbuilt">
 							<div class="med-thumb media">
@@ -87,25 +99,54 @@
 							</div>
 						</div>
 					</div>
-				</li>
-			</ul>
-			<div class="moyn__footer">
-				Copyright &copy 2018 | <span><img src="http://i65.tinypic.com/2rny2pu.png" alt="moyn-logo" height="16"></span>
+				</div>
 			</div>
-			<div class="moyn__slider">
-				<div class="overlay">
-					<div class="carousel">
-						<img src="http://placehold.it/2000x2000" alt="">
-						<img src="http://placehold.it/2000x2000" alt="">
-						<img src="http://placehold.it/2000x2000" alt="">
-						<img src="http://placehold.it/2000x2000" alt="">
+
+
+			<!-- menu mobile -->
+			<div class="moyn__menu--mobile">
+				<div class="collapse" id="navbarToggleExternalContent">
+					<div>
+						<div class="moyn__menu-item" id="profile">PROFILE</div>
+						<div class="moyn__menu-item" id="projects">PROJECTS</div>
+						<div class="moyn__menu-item" id="contact">CONTACT</div>
+						<div class="moyn__menu-item" id="news">NEWS</div>
+						<div class="moyn__menu-item" id="shop">SHOP</div>
 					</div>
 				</div>
 			</div>
+
+			<div class="moyn__content--mobile" id="content-mobile"></div>
+
+			<!-- footer -->
+			<div class="moyn__footer">
+				Copyright &copy 2018 | <span><img src="http://i65.tinypic.com/2rny2pu.png" alt="moyn-logo" height="16"></span>
+			</div>
+
+			<!-- carousel -->
+			<div class="popup-slider" id="popup-slider">
+				<div class="overlay">
+					<div class="carousel" id="carousel">
+						<img src="http://placehold.it/800x800" alt="">
+						<img src="http://placehold.it/700x800" alt="">
+						<img src="http://placehold.it/800x900" alt="">
+						<img src="http://placehold.it/800x800" alt="">
+					</div>
+					<div class="navigation" id="carousel-nav">
+						<div class="navigation__prev" id="carousel-nav-prev"><</div>
+						<div class="navigation__next" id="carousel-nav-next">></div>
+					</div>
+					<div class="close" id="carousel-close">x</div>
+				</div>
+			</div>
+			<!-- carousel -->
 		</div>
 		<script>
 			var baseUrl = "<?php echo base_url(); ?>"
 		</script>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 		<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 		<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/moyn.js"></script>
--- assets/js/moyn.js
+++ assets/js/moyn.js
@@ -3,6 +3,68 @@ Array.prototype.chunk = function ( n ) {
 	return [ this.slice( 0, n ) ].concat( this.slice(n).chunk(n) );
 };
 
+const fetchDataProject = function(queryType) {
+	$("#proj-gallery").empty()
+	
+	$.ajax({
+		type: "POST",
+		url: baseUrl + 'moyn/project',
+		data: {type: queryType.toString()},
+		success: function(result){
+			console.log(result)
+			var json = $.parseJSON(result)
+			var arrThumb = []
+			$.each(json, function(index, item) {
+				var id = "", name = "", type = ""
+				switch (queryType) {
+					case "unbuilt_project":
+						id = item.id_up
+						name = item.name_up
+						type = item.type_up
+						break;
+					
+					case "my_studio":
+						id = item.id_studio
+						name = item.name_studio
+						type = item.type_studio
+						break;
+					
+					case "realized_project":
+						id = item.id_rp
+						name = item.name_rp
+						type = item.type_rp
+						break;
+				
+					default:
+						break;
+				}
+				var thumbDOM = "<div class='proj-gallery__thumb' id='"+id+"' >"
+					thumbDOM += "<div class='proj-gallery__prev' style='background-image: url("+"http://placehold.it/100x100"+")' />"
+					thumbDOM += "<div class='proj-gallery__body'>"
+						thumbDOM += "<div class='proj-gallery__title'>"+name+"</div>"
+						thumbDOM += "<div class='proj-gallery__desc'>"+type+"</div>"
+					thumbDOM += "</div>"
+				thumbDOM += "</div>"
+				
+				arrThumb.push(thumbDOM)								
+			})
+
+			var isMobile = $(window).width() <= 576
+			var chunkSize = $(window).width() > 992 ? 8 : 4
+			var groupThumb = arrThumb.chunk(chunkSize)
+
+			$.each(groupThumb, function(indexGroup, group) {
+				var groupDOM = "<div class='proj-gallery__column'><div class='proj-gallery__column-inner'>"
+				$.each(group, function(index, item) {
+					groupDOM += item
+				})
+				groupDOM += "</div></div>"
+				$("#proj-gallery").append(groupDOM).fadeIn()
+			})
+		}
+	});
+};
+
 $(document).ready(function(){
 	// PROFILE
 	$(".moyn__founders-img--left > img").hover(function(){
@@ -20,145 +82,81 @@ $(document).ready(function(){
 					menus[i].classList.remove('active');
 
 		this.className += " active"
-		// if (this.id == "projects") {$("#nav-studio").click()}
+
+		$(".navbar-toggler").click()		
+		$("#content-mobile").empty()
+
+		if($(window).width() <= 576) {
+			switch (this.id) {
+				case "profile":
+					$("#content-mobile").append($("<div class='c-white'>PROFILE</div>")).fadeIn()
+					$("#content-mobile").append($("#content-profile")[0]).fadeIn()
+					$("#content-profile").css({
+						"opacity": 1,
+						"display": "block",
+						"max-width": "100%",
+						"text-align": "left",
+						"position": "initial"
+					})
+					break;
+				case "projects":
+					$("#content-mobile").append($("<div class='c-white'>PROFILE</div>")).fadeIn()
+					$("#content-mobile").append($("#content-projects")[0]).fadeIn()
+					$("#content-profile").css({
+						"opacity": 1,
+						"display": "block",
+						"max-width": "100%",
+						"text-align": "left",
+						"position": "initial"
+					})
+					break
+				case "contact":			
+					$("#content-mobile").append($("<div class='c-white'>CONTACT</div>")).fadeIn()			
+					$("#content-mobile").append($("#content-contact")[0]).fadeIn()
+					$("#content-contact").css({
+						"opacity": 1,
+						"display": "block",
+						"max-width": "100%",
+						"text-align": "left",
+						"position": "initial"
+					})
+					break;
+			
+				default:
+					break;
+			}
+		}
 	})
 
-	// AJAX PROJECT
+	// FOOTER
+	$(".moyn__footer")[0].style.top = window.innerHeight - $(".moyn__footer")[0].clientHeight - 8
+
+
+	// PROJECT
 	$("#nav-unbuilt").click(function() {
-		$("#proj-gallery").empty()
-		
-		// toggle navigator class 
 		$("#nav-studio").removeClass("active")
 		$("#nav-realized").removeClass("active")
 		$("#nav-unbuilt").addClass("active")
 
-		// fetch data
-		var type = $("#nav-unbuilt").text()
-		$.ajax({
-			type: "POST",
-			url: baseUrl+'moyn/project',
-			data: {type: "unbuilt_project"},
-			success: function(result){
-				var json = $.parseJSON(result)
-				var arrThumb = []
-				$.each(json, function(index, item) {
-					var thumb_up = "<div class='proj-gallery__thumb' id='"+item.id_up+"' >"
-						thumb_up += "<div class='proj-gallery__prev' style='background-image: url("+"http://placehold.it/100x100"+")' />"
-						thumb_up += "<div class='proj-gallery__body'>"
-							thumb_up += "<div class='proj-gallery__title'>"+item.name_up+"</div>"
-							thumb_up += "<div class='proj-gallery__desc'>"+item.type_up+"</div>"
-						thumb_up += "</div>"
-					thumb_up += "</div>"
-					
-					arrThumb.push(thumb_up)	
-				})
-
-				var chunkSize = $(window).width() > 992 ? 8 : 4
-				var groupThumb = arrThumb.chunk(chunkSize)
-
-				$.each(groupThumb, function(indexGroup, group) {
-					var groupDOM = "<div class='proj-gallery__column'><div class='proj-gallery__column-inner'>"
-					$.each(group, function(index, item) {
-						groupDOM += item
-					})
-					groupDOM += "</div></div>"
-					$("#proj-gallery").append(groupDOM).fadeIn()
-				})
-			}
-		});
+		fetchDataProject("unbuilt_project")
 	})
 
 	$("#nav-studio").click(function() {
-		$("#proj-gallery").empty()
-
-		// toggle navigator class 
+	
 		$("#nav-studio").addClass("active")
 		$("#nav-realized").removeClass("active")
 		$("#nav-unbuilt").removeClass("active")
 
-		// fetch data
-		var type = $("#nav-studio").text()
-		$.ajax({
-			type: "POST",
-			url: baseUrl + 'moyn/project',
-			data: {type: "studio_project"},
-			success: function(result){
-				var json = $.parseJSON(result)
-				var arrThumb = []
-				$.each(json, function(index, item) {
-					var thumb_sp = "<div class='proj-gallery__thumb' id='"+item.id_studio+"' >"
-						thumb_sp += "<div class='proj-gallery__prev' style='background-image: url("+"http://placehold.it/100x100"+")' />"
-						thumb_sp += "<div class='proj-gallery__body'>"
-							thumb_sp += "<div class='proj-gallery__title'>"+item.name_studio+"</div>"
-							thumb_sp += "<div class='proj-gallery__desc'>"+item.type_studio+"</div>"
-						thumb_sp += "</div>"
-					thumb_sp += "</div>"
-					
-					arrThumb.push(thumb_sp)								
-				})
-
-				var chunkSize = $(window).width() > 992 ? 8 : 4
-				var groupThumb = arrThumb.chunk(chunkSize)
-
-				$.each(groupThumb, function(indexGroup, group) {
-					var groupDOM = "<div class='proj-gallery__column'><div class='proj-gallery__column-inner'>"
-					$.each(group, function(index, item) {
-						groupDOM += item
-					})
-					groupDOM += "</div></div>"
-					$("#proj-gallery").append(groupDOM).fadeIn()
-				})
-			}
-		});
+		fetchDataProject("my_studio")
 	})
 
 	$("#nav-realized").click(function() {
-		$("#proj-gallery").empty()
-
-		// toggle navigator class 
 		$("#nav-studio").removeClass("active")
 		$("#nav-realized").addClass("active")
 		$("#nav-unbuilt").removeClass("active")
 
-		// fetch data 
-		var type = $("#nav-realized").text()
-		$.ajax({
-			type: "POST",
-			url: baseUrl + 'moyn/project',
-			data: {type: "realized_project"},
-			success: function(result){
-				var json = $.parseJSON(result)
-				var arrThumb = []
-				$.each(json, function(index, item) {
-					var thumb_rp = "<div class='proj-gallery__thumb' id='"+item.id_rp+"' >"
-						thumb_rp += "<div class='proj-gallery__prev' style='background-image: url("+"http://placehold.it/100x100"+")' />"
-						thumb_rp += "<div class='proj-gallery__body'>"
-							thumb_rp += "<div class='proj-gallery__title'>"+item.name_rp+"</div>"
-							thumb_rp += "<div class='proj-gallery__desc'>"+item.type_rp+"</div>"
-						thumb_rp += "</div>"
-					thumb_rp += "</div>"
-					
-					arrThumb.push(thumb_rp)
-					// $("#proj-gallery").append(thumb_rp).fadeIn()
-				})
-
-				var chunkSize = $(window).width() > 992 ? 8 : 4
-				var groupThumb = arrThumb.chunk(chunkSize)
-
-				$.each(groupThumb, function(indexGroup, group) {
-					var groupDOM = "<div class='proj-gallery__column'><div class='proj-gallery__column-inner'>"
-					$.each(group, function(index, item) {
-						groupDOM += item
-					})
-					groupDOM += "</div></div>"
-					$("#proj-gallery").append(groupDOM).fadeIn()
-				})
-				// <div class='proj-gallery__column'>
-			}
-		});
+		fetchDataProject("realized_project")		
 	})
-
-
 
 	// GALLERY
 	var columnWidth = $(window).width() > 992 ? 508 : 258
@@ -177,5 +175,34 @@ $(document).ready(function(){
 			$("#proj-gallery").animate({scrollLeft: columnWidth * currentPosition}, 500)
 			currentPosition++
 		}
+	})
+
+	// CAROUSEL
+	var columnWidth = $(window).width() > 576 ? 488 : 308
+	var currentPosition = 1
+	
+	$("#carousel-nav-prev").click(function() {
+		console.log("asd")
+		var limitLeft = 1
+		if (currentPosition > limitLeft) {
+			currentPosition -= 1		
+			$("#carousel").animate({scrollLeft: columnWidth * currentPosition - columnWidth}, 500)
+		}
+	})
+	$("#carousel-nav-next").click(function() {
+		var limitRight = $("#carousel")[0].scrollWidth / columnWidth
+		if (currentPosition < limitRight) {
+			$("#carousel").animate({scrollLeft: columnWidth * currentPosition}, 500)
+			currentPosition++
+		}
+	})
+
+	$(".proj-gallery__thumb").click(function() {
+		alert("anu")
+		$("#popup-slider").show()		
+	})
+	
+	$("#carousel-close").click(function() {
+		$("#popup-slider").hide()
 	})
 });
