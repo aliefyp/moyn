9d58e6c81ac9f9ce2ec217ba631aecde2299659c
Nakama <aliefyp@gmail.com>
2018-02-08 21:30:59 +0700

    re-routing

--- application/views/home.php
+++ application/views/home.php
@@ -7,9 +7,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/moyn.css">
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/slick/slick.css"/>
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/slick/slick-theme.css"/>
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/owl/css/owl.carousel.css"/>
 	</head>
 
 	<body>
@@ -27,7 +24,8 @@
 
 			<!-- menu desktop -->
 			<div class="moyn__menu">
-				<div class="moyn__menu-item" id="profile">PROFILE
+				<div class="moyn__menu-item  <?php if($this->uri->segment(1)=="profile"){echo 'active';}?>" id="profile">
+					<a href="<?php echo base_url(); ?>profile">PROFILE</a>
 					<div class="moyn__menu-content" id="content-profile">
 						<p>Established in 2018, moyn is an architectural design studio based on the countryside of central Java.<br />
 						the balance between spaces inside and outside, environment and architecture, human and architecture.<br />
@@ -56,7 +54,8 @@
 						</div>
 					</div>
 				</div>
-				<div class="moyn__menu-item" id="projects">PROJECTS
+				<div class="moyn__menu-item <?php if($this->uri->segment(1)=="projects"){echo 'active';}?>" id="projects">
+					<a href="<?php echo base_url(); ?>projects">PROJECTS</a>
 					<div class="moyn__content-nav justify-content-end" id="content-project">
 						<div class="proj-nav">
 							<div class="proj-nav__item" id="nav-unbuilt" >&ndash;</div>
@@ -65,13 +64,14 @@
 						</div>
 					</div>
 					<div class="moyn__menu-content">
-						<!-- <div class="moyn__gallery" id="proj-gallery"></div> -->
-						<div class="proj-gallery" id="proj-gallery"></div>
-						<div class="proj-next" id="gallery-next">&gt;</div>
-						<div class="proj-prev" id="gallery-prev">&lt;</div>
+						<!-- <div class="moyn__gallery" id="gallery"></div> -->
+						<div class="gallery" id="gallery-project"></div>
+						<div class="proj-next" id="gallery-project-next">&gt;</div>
+						<div class="proj-prev" id="gallery-project-prev">&lt;</div>
 					</div>
 				</div>
-				<div class="moyn__menu-item" id="contact">CONTACT
+				<div class="moyn__menu-item <?php if($this->uri->segment(1)=="contact"){echo 'active';}?>" id="contact">
+					<a href="<?php echo base_url(); ?>contact">CONTACT</a>
 					<div class="moyn__menu-content ta-right" id="content-contact">
 						<div class="mb-16">
 							<div>Jl.Carikan Gondosuli</div>
@@ -80,24 +80,41 @@
 							<div>7.5676, 110.2842 E</div>
 						</div>
 						<div>
-							<div>+62 81 235 414818</div>
+							<div>+6281 3900 7 2121</div>
 							<div>studio.moyn@gmail.com</div>
 						</div>
 					</div>
 				</div>
-				<div class="moyn__menu-item" id="news">NEWS
+				<div class="moyn__menu-item <?php if($this->uri->segment(1)=="news"){echo 'active';}?>" id="news">
+					<a href="<?php echo base_url(); ?>news">NEWS</a>			
 				</div>
-				<div class="moyn__menu-item" id="shop">SHOP
+				<div class="moyn__menu-item <?php if($this->uri->segment(1)=="shop"){echo 'active';}?>" id="shop">
+					<a href="<?php echo base_url(); ?>shop">SHOP</a>		
 					<div class="moyn__menu-content">
-						<div class="moyn__gallery" id="gallery-unbuilt">
-							<div class="med-thumb media">
-								<div class="med-thumb__image" style="background-image: url('http://placehold.it/100x100')"></div>
-								<div class="med-thumb__body media-body">
-									<div class="med-thumb__title">ASDSAD</div>
-									<div class="med-thumb__desc">asdsad</div>
-								</div>
-							</div>
-						</div>
+						<div class="gallery" id="gallery-shop"></div>
+						<div class="proj-next" id="gallery-shop-next">&gt;</div>
+						<div class="proj-prev" id="gallery-shop-prev">&lt;</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="moyn__extra-content <?php if($this->uri->segment(1)=="product"){echo 'active';}?>">
+				<div class="purchasing">
+					<img src="<?php echo $product->url_img_item ?>" alt="<?php echo $product->name_item ?>" class="product-img" />
+					<div class="product-desc">
+						<div class="title"><?php echo $product->name_item ?></div>
+						<div class="description"><?php echo $product->deskripsi_item ?></div>
+						<button class="purchase" onclick="goBack()">BACK</button>
+						<button class="purchase">BUY</button>
+						<form action="" class="mt-32" id="purchase-form">
+							<div class="mb-16">Please fill following form</div>
+							<input class="mb-8" type="text" placeHolder="Fullname" required autofocus />
+							<input class="mb-8" type="text" placeHolder="Email" required />
+							<input class="mb-8" type="text" placeHolder="Quantity" required />
+							<input class="mb-8" type="text" placeHolder="Phone" required />
+							<textarea class="mb-8" cols="30" rows="3" placeHolder="Shipping Address" required></textarea>
+							<textarea class="mb-8" cols="30" rows="3" placeHolder="Message" required></textarea>
+						</form>
 					</div>
 				</div>
 			</div>
@@ -107,15 +124,24 @@
 			<div class="moyn__menu--mobile">
 				<div class="collapse" id="navbarToggleExternalContent">
 					<div>
-						<div class="moyn__menu-item" id="profile">PROFILE</div>
-						<div class="moyn__menu-item" id="projects">PROJECTS</div>
-						<div class="moyn__menu-item" id="contact">CONTACT</div>
-						<div class="moyn__menu-item" id="news">NEWS</div>
-						<div class="moyn__menu-item" id="shop">SHOP</div>
+						<div class="moyn__menu-item <?php if($this->uri->segment(1)=="profile" || $this->uri->segment(1)==""){echo 'active';}?>" id="profile">
+							<a href="<?php echo base_url(); ?>profile">PROFILE</a>
+						</div>
+						<div class="moyn__menu-item <?php if($this->uri->segment(1)=="projects"){echo 'active';}?>" id="projects">
+							<a href="<?php echo base_url(); ?>projects">PROJECTS</a>
+						</div>
+						<div class="moyn__menu-item <?php if($this->uri->segment(1)=="contact"){echo 'active';}?>" id="contact">
+							<a href="<?php echo base_url(); ?>contact">CONTACT</a>
+						</div>
+						<div class="moyn__menu-item <?php if($this->uri->segment(1)=="news"){echo 'active';}?>" id="news">
+							<a href="<?php echo base_url(); ?>news">NEWS</a>
+						</div>
+						<div class="moyn__menu-item <?php if($this->uri->segment(1)=="shop"){echo 'active';}?>" id="shop">
+							<a href="<?php echo base_url(); ?>shop">SHOP</a>
+						</div>
 					</div>
 				</div>
 			</div>
-
 			<div class="moyn__content--mobile" id="content-mobile"></div>
 
 			<!-- footer -->
@@ -123,8 +149,17 @@
 				Copyright &copy 2018 | <span><img src="http://i65.tinypic.com/2rny2pu.png" alt="moyn-logo" height="16"></span>
 			</div>
 
+			<!--  -->
+			<!-- <div class="popup">
+				<div class="overlay">
+					<div class="modal-form purchasing">
+					</div>
+					<div class="close" id="carousel-close">x</div>					
+				</div>
+			</div> -->
+
 			<!-- carousel -->
-			<div class="popup-slider" id="popup-slider">
+			<div class="popup" id="popup-slider">
 				<div class="overlay">
 					<div class="carousel" id="carousel">
 						<img src="http://placehold.it/800x800" alt="">
@@ -140,9 +175,14 @@
 				</div>
 			</div>
 			<!-- carousel -->
+			
 		</div>
 		<script>
 			var baseUrl = "<?php echo base_url(); ?>"
+			var route = "<?php echo $this->uri->segment(1); ?>"
+			function goBack() {
+				window.history.back();
+			}
 		</script>
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -150,7 +190,5 @@
 		<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 		<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/moyn.js"></script>
-		<script type="text/javascript" src="<?php echo base_url(); ?>assets/slick/slick.min.js"></script>
-		<script type="text/javascript" src="<?php echo base_url(); ?>assets/owl/js/owl.carousel.js"></script>
 	</body>
 </html>
--- assets/js/moyn.js
+++ assets/js/moyn.js
@@ -1,50 +1,53 @@
+var galleryPosition = 1;
+var carouselPosition = 1;
+
 Array.prototype.chunk = function ( n ) {
 	if ( !this.length ) { return []; }
 	return [ this.slice( 0, n ) ].concat( this.slice(n).chunk(n) );
 };
 
 const fetchDataProject = function(queryType) {
-	$("#proj-gallery").empty()
-
-	console.log(queryType)
+	$("#gallery-project").empty()
 	
 	$.ajax({
 		type: "POST",
-		url: baseUrl + 'moyn/project',
+		url: baseUrl + 'projects/' + queryType,
 		data: {type: queryType.toString()},
 		success: function(result){
-			console.log(result)
 			var json = $.parseJSON(result)
 			var arrThumb = []
 			$.each(json, function(index, item) {
-				var id = "", name = "", type = ""
+				var id = "", name = "", type = "", image = ""
 				switch (queryType) {
-					case "unbuilt_project":
+					case "unbuilt":
 						id = item.id_up
 						name = item.name_up
 						type = item.type_up
+						image = item.url_iup
 						break;
 					
-					case "my_studio":
+					case "studio":
 						id = item.id_studio
 						name = item.name_studio
 						type = item.type_studio
+						image = item.url_istd
 						break;
 					
-					case "realized_project":
+					case "realized":
 						id = item.id_rp
 						name = item.name_rp
 						type = item.type_rp
+						image = item.url_irp
 						break;
 				
 					default:
 						break;
 				}
-				var thumbDOM = "<div class='proj-gallery__thumb' id='"+id+"' >"
-					thumbDOM += "<div class='proj-gallery__prev' style='background-image: url("+"http://placehold.it/100x100"+")' />"
-					thumbDOM += "<div class='proj-gallery__body'>"
-						thumbDOM += "<div class='proj-gallery__title'>"+name+"</div>"
-						thumbDOM += "<div class='proj-gallery__desc'>"+type+"</div>"
+				var thumbDOM = "<div class='gallery__thumb' id='proj-"+id+"' >"
+					thumbDOM += "<div class='gallery__prev' style='background-image: url("+image+")' />"
+					thumbDOM += "<div class='gallery__body'>"
+						thumbDOM += "<div class='gallery__title'>"+name+"</div>"
+						thumbDOM += "<div class='gallery__desc'>"+type+"</div>"
 					thumbDOM += "</div>"
 				thumbDOM += "</div>"
 				
@@ -56,18 +59,121 @@ const fetchDataProject = function(queryType) {
 			var groupThumb = arrThumb.chunk(chunkSize)
 
 			$.each(groupThumb, function(indexGroup, group) {
-				var groupDOM = "<div class='proj-gallery__column'><div class='proj-gallery__column-inner'>"
+				var groupDOM = "<div class='gallery__column'><div class='gallery__column-inner'>"
 				$.each(group, function(index, item) {
 					groupDOM += item
 				})
 				groupDOM += "</div></div>"
-				$("#proj-gallery").append(groupDOM).fadeIn()
+				$("#gallery-project").append(groupDOM).fadeIn()
 			})
 		}
 	});
 };
 
+const fetchDataShop = function() {
+	$("#gallery-shop").empty()
+	
+	$.ajax({
+		type: "POST",
+		url: baseUrl + 'shop/products' ,
+		success: function(result){
+			
+			var json = $.parseJSON(result)
+			var arrThumb = []
+			$.each(json, function(index, item) {
+				var thumbDOM = "<a href='"+baseUrl+"product?id="+item.id_item+"'>"
+					thumbDOM += "<div class='gallery__thumb' id='product-"+item.id_item+"' >"
+						thumbDOM += "<div class='gallery__prev' style='background-image: url("+item.url_img_item+")' />"
+						thumbDOM += "<div class='gallery__body'>"
+							thumbDOM += "<div class='gallery__title'>"+item.name_item+"</div>"
+							thumbDOM += "<div class='gallery__desc'>"+item.price_item+"</div>"
+						thumbDOM += "</div>"
+					thumbDOM += "</div>"
+				thumbDOM += "</a>"
+				
+				arrThumb.push(thumbDOM)								
+			})
+
+			var isMobile = $(window).width() <= 576
+			var chunkSize = $(window).width() > 992 ? 8 : 4
+			var groupThumb = arrThumb.chunk(chunkSize)
+
+			$.each(groupThumb, function(indexGroup, group) {
+				var groupDOM = "<div class='gallery__column'><div class='gallery__column-inner'>"
+				$.each(group, function(index, item) {
+					groupDOM += item
+				})
+				groupDOM += "</div></div>"
+				$("#gallery-shop").append(groupDOM).fadeIn()
+			})
+		}
+	});
+};
+
+const displaySlider = function(table, proj_id, table_id) {
+	$("#carousel").empty()
+	
+	$.ajax({
+		type: "POST",
+		url: baseUrl + 'projects/slider' ,
+		data: {
+			table: table,
+			proj_id: proj_id,
+			table_id: table_id,
+		},
+		success: function(result){
+			var json = $.parseJSON(result)
+			var arrThumb = []
+			$.each(json, function(index, item) {
+				var imgUrl = ""
+				switch (table) {
+					case "img_unbuilt_project":
+						imgUrl = item.url_iup
+						break;
+					
+					case "img_studio":
+						imgUrl = item.url_istd
+						break;
+					
+					case "img_realized_project":
+						imgUrl = item.url_irp
+						break;
+					
+					case "img_shop_item":
+						imgUrl = item.url_img_item
+						break;
+				
+					default:
+						imgUrl = ""
+						break;
+				}
+				
+				arrThumb.push("<img src='"+imgUrl+"' alt='"+imgUrl+"' />")								
+			})
+
+			$("#carousel").append(arrThumb)
+			$("#popup-slider").fadeIn()
+		}
+	});
+};
+
 $(document).ready(function(){
+	var tableImgName = null, tableIdName = null
+	
+	// INITIALIZE GALLERY
+	if (route === "projects") {
+		tableImgName = "img_studio"
+		tableIdName = "id_studio"
+		galleryPosition = 1
+		fetchDataProject("studio")
+	}
+	if (route === "shop") {
+		tableImgName = "img_shop_item"
+		tableIdName = "id_item"
+		galleryPosition = 1
+		fetchDataShop()
+	}
+	
 	// PROFILE
 	$(".moyn__founders-img--left > img").hover(function(){
 		$(".moyn__founders-img--right > .desc").fadeToggle( "fast", "linear" )
@@ -77,31 +183,15 @@ $(document).ready(function(){
 		$(".moyn__founders-img--left > .desc").fadeToggle( "fast", "linear" )
 	})
 
+
 	// MENU
-	$(".moyn__menu-item").click(function() {
-		var menus = document.getElementsByClassName(this.className)
-		for (var i = 0; i < menus.length; i++)
-					menus[i].classList.remove('active');
-
-		this.className += " active"
-
-
-		// if(this.id == "projects"){
-		// 	$(".proj-nav__item").each(function(item) {
-		// 		console.log(item.hasClass(""))
-		// 		if(item.hasClass("")) {
-		// 			item.click()
-		// 		}
-		// 	})
-		// }
-
-
+	// $(".moyn__menu-item").click(function() {
 		// mobile view handling
-		$(".navbar-toggler").click()		
-		$("#content-mobile").empty()
 
 		if($(window).width() <= 576) {
-			switch (this.id) {
+			// $(".navbar-toggler").click()		
+			$("#content-mobile").empty()
+			switch (route) {
 				case "profile":
 					$("#content-mobile").append($("<div class='c-white'>PROFILE</div>")).fadeIn()
 					$("#content-mobile").append($("#content-profile")[0]).fadeIn()
@@ -114,9 +204,9 @@ $(document).ready(function(){
 					})
 					break;
 				case "projects":
-					$("#content-mobile").append($("<div class='c-white'>PROFILE</div>")).fadeIn()
-					$("#content-mobile").append($("#content-projects")[0]).fadeIn()
-					$("#content-profile").css({
+					$("#content-mobile").append($("<div class='c-white'>PROJECT</div>")).fadeIn()
+					$("#content-mobile").append($("#content-project")[0]).fadeIn()
+					$("#content-project").css({
 						"opacity": 1,
 						"display": "block",
 						"max-width": "100%",
@@ -140,7 +230,7 @@ $(document).ready(function(){
 					break;
 			}
 		}
-	})
+	// })
 
 	// FOOTER
 	$(".moyn__footer")[0].style.top = window.innerHeight - $(".moyn__footer")[0].clientHeight - 8
@@ -153,16 +243,19 @@ $(document).ready(function(){
 		$("#nav-realized").removeClass("active")
 		$("#nav-unbuilt").addClass("active")
 
-		fetchDataProject("unbuilt_project")
+		tableImgName = "img_unbuilt_project"
+		tableIdName = "id_up"
+		fetchDataProject("unbuilt")
 	})
 
 	$("#nav-studio").click(function() {
-	
 		$("#nav-studio").addClass("active")
 		$("#nav-realized").removeClass("active")
 		$("#nav-unbuilt").removeClass("active")
 
-		fetchDataProject("my_studio")
+		tableImgName = "img_studio"
+		tableIdName = "id_studio"
+		fetchDataProject("studio")
 	})
 
 	$("#nav-realized").click(function() {
@@ -170,54 +263,63 @@ $(document).ready(function(){
 		$("#nav-realized").addClass("active")
 		$("#nav-unbuilt").removeClass("active")
 
-		fetchDataProject("realized_project")		
+		tableImgName = "img_realized_project"
+		tableIdName = "id_rp"
+		fetchDataProject("realized")		
 	})
 
+
+
 	// GALLERY
-	var columnWidth = $(window).width() > 992 ? 516 : 266
-	var currentPosition = 1
+	var columnWidth = $(window).width() > 992 ? 508 : 258
 	
-	$("#gallery-prev").click(function() {
+	$("#gallery-project-prev").click(function() {
 		var limitLeft = 1
-		if (currentPosition > limitLeft) {
-			currentPosition -= 1		
-			$("#proj-gallery").animate({scrollLeft: columnWidth * currentPosition - columnWidth}, 500)
+		if (galleryPosition > limitLeft) {
+			$("#gallery-project").animate({scrollLeft: columnWidth * galleryPosition - columnWidth}, 500)
+			
+			if (galleryPosition == limitLeft) {this.hide()}			
 		}
 	})
-	$("#gallery-next").click(function() {
-		var limitRight = $("#proj-gallery")[0].scrollWidth / columnWidth
-		if (currentPosition < limitRight) {
-			$("#proj-gallery").animate({scrollLeft: columnWidth * currentPosition}, 500)
-			currentPosition++
+	$("#gallery-project-next").click(function() {
+		var limitRight = $("#gallery-project")[0].scrollWidth / columnWidth
+		if (galleryPosition < limitRight) {
+			$("#gallery-project").animate({scrollLeft: columnWidth * galleryPosition}, 500)
+			galleryPosition++
+
+			if (galleryPosition == limitRight) {this.hide()}
 		}
 	})
 
 	// CAROUSEL
 	var columnWidth = $(window).width() > 576 ? 488 : 308
-	var currentPosition = 1
+
+	$(document).on("click", ".gallery__thumb", function() {
+		var table = tableImgName
+		var proj_id = this.id.split("-")[1]
+		var table_id  = tableIdName
+
+		displaySlider(table, proj_id, table_id)
+	})
 	
 	$("#carousel-nav-prev").click(function() {
-		console.log("asd")
 		var limitLeft = 1
-		if (currentPosition > limitLeft) {
-			currentPosition -= 1		
-			$("#carousel").animate({scrollLeft: columnWidth * currentPosition - columnWidth}, 500)
+		if (carouselPosition > limitLeft) {
+			carouselPosition -= 1		
+			$("#carousel").animate({scrollLeft: columnWidth * carouselPosition - columnWidth}, 500)
 		}
 	})
 	$("#carousel-nav-next").click(function() {
 		var limitRight = $("#carousel")[0].scrollWidth / columnWidth
-		if (currentPosition < limitRight) {
-			$("#carousel").animate({scrollLeft: columnWidth * currentPosition}, 500)
-			currentPosition++
+		if (carouselPosition < limitRight) {
+			$("#carousel").animate({scrollLeft: columnWidth * carouselPosition}, 500)
+			carouselPosition++
 		}
 	})
 
-	$(".proj-gallery__thumb").click(function() {
-		alert("anu")
-		$("#popup-slider").show()		
-	})
-	
 	$("#carousel-close").click(function() {
+		carouselPosition = 1
+		$("#carousel").scrollLeft(0)
 		$("#popup-slider").hide()
 	})
 });
